f9e0f8e71504bf2da127ce073575d4348a264f16
Gabriel Desilets <gabriel.desilets@gsoft.com>
2024-02-23 08:49:49 -0500

    added configs

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -2,13 +2,27 @@
 # --------------------------------------
 alias ls="exa --long --time-style=long-iso --git --group-directories-first --header --links"
 alias ll='ls -la'
-alias ~='cd ~'
 alias tree='exa -T'
 alias cat='bat'
 alias c='clear'
+alias g='git'
+
+# Navigation aliases
+# -----------------------------------
+alias ~='cd ~'
+alias ..='cd ..'
+alias lms='cd ${HOME}/dev/didacte'
 
 # Editor (always forward to Neovim!)
 alias vi='nvim'
 alias vim='nvim'
+
+# TMUX
+# -------------------------------------
+alias tmat='tmux attach-session -t'
+alias tma='tmux attach-session'
+alias tml='tmux ls'
+alias tmns='tmux new-session -s'
+alias tmka='tmux kill-session -a'
 
 
